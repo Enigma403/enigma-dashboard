c7e5f137772d7870d8bda9dc8c236defcdd91246
Klaudiusz <muminowy123@gmail.com>
2023-05-02 13:53:01 +0200

    EN-2 Added user model with password hashing function

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,32 +1,36 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
-// User Model
-const userSchema = new mongoose.Schema({
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
     name: {
-      type: String,
-      required: true,
+        type:String,
+        required: true,
+        unique: true,
     },
     email: {
         type: String,
         required: true,
-        unique: true,
-        lowercase: true,
+        unique: true
     },
     password: {
         type: String,
-        required: true,
-    },
-});
-
-userSchema.pre('save', async function (next) {
-    const user = this;
-    if (user.isModified('password')) {
-        user.password = await bcrypt.hash(user.password, 8);
+        required: true
     }
-    next();
 });
 
-const User = mongoose.model('User', userSchema);
-
-module.exports = User;
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(this.password, salt);
+        this.password = hash;
+        return next();
+    } catch (err) {
+        return next(err);
+    }
+});
+module.exports = mongoose.model('User', UserSchema);
