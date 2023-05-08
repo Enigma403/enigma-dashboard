1dd227ba117689b08139937bc35eafb7ed7c7545
Klaudiusz <muminowy123@gmail.com>
2023-05-08 14:49:34 +0200

    EN-7 Update server configuration and middleware for authentication

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const cors = require('cors')
+const passport = require('passport');
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const crypto = require('crypto')
 const AuthRoutes = require("./routes/AuthRoutes");
 const db = require('./config/database');
 const {config} = require('./config/config');
 const DashRoutes = require("./routes/DashRoutes");
-const bodyParser = require("body-parser");
-const cors = require('cors')
 
-const session = require("express-session");
+
 
 const app = express();
 
@@ -21,11 +24,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(bodyParser.json());
 
+const secretKey = crypto.randomBytes(64).toString('hex');
+
 app.use(session({
-    secret: 'my-secret-key',
-    resave: false,
-    saveUninitialized: true
+    secret: secretKey,
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 30 * 24 * 60 * 60 * 1000
+    }
 }));
+
 
 // Routes
 app.use('/',AuthRoutes);
